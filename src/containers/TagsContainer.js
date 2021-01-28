23ce07e172c95a5005d3a019e35c8b2f41108a67
r00bal <hal9000@MacBook-Pro-r00bal.local>
2021-01-28 15:47:49 +0100

    add onScroll

--- src/containers/TagsContainer.js
+++ src/containers/TagsContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
@@ -8,6 +8,32 @@ const CategoriesContainer = styled.div`
   width: 100%;
   height: 50px;
   max-width: 1300px;
+  position: relative;
+  &:before,
+  &:after {
+    opacity: 0;
+    transition: opacity 0.2s ease-in-out;
+    content: '';
+    display: block;
+    pointer-events: none;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    z-index: 1;
+    width: 100px;
+  }
+  &:before {
+    left: 0;
+    width: 100px;
+    background: linear-gradient(270deg, hsla(0, 0%, 100%, 0) 0, #fff 95%, #fff);
+    opacity: ${({ blur }) => (blur.before ? 1 : 0)};
+  }
+  &:after {
+    z-index: 1;
+    right: 0;
+    background: linear-gradient(90deg, hsla(0, 0%, 100%, 0) 0, #fff 95%, #fff);
+    opacity: ${({ blur }) => (blur.after ? 1 : 0)};
+  }
 `;
 
 const CategoriesInnerWrapper = styled.div`
@@ -40,22 +66,43 @@ const LinkButton = styled(Link)`
   text-transform: capitalize;
   padding: 13px;
   font-size: 14px;
+
   text-align: center;
   border-radius: 5px;
   border: 1px solid #d1d1d1;
-  color: inherit;
+  color: #949494;
   cursor: pointer;
   text-decoration: none;
-  &:focus {
+  &:focus,
+  &:hover {
     outline: none;
     border: 1px solid black;
+    color: #000000;
   }
 `;
 
 export default function TagsContainer({ categories }) {
+  const [blur, setBlur] = useState({ before: false, after: true });
+
+  const handleScrool = (event) => {
+    const { scrollLeft, clientWidth, scrollWidth } = event.currentTarget;
+    console.log(scrollLeft, clientWidth, scrollWidth);
+    if (scrollLeft === 0) {
+      setBlur((prev) => ({ ...prev, ...{ before: false } }));
+    }
+    if (scrollLeft > 0) {
+      setBlur((prev) => ({ ...prev, ...{ before: true } }));
+    }
+    if (scrollWidth - clientWidth === scrollLeft) {
+      setBlur((prev) => ({ ...prev, ...{ after: false } }));
+    }
+    if (clientWidth < scrollWidth && scrollWidth - clientWidth !== scrollLeft) {
+      setBlur((prev) => ({ ...prev, ...{ after: true } }));
+    }
+  };
   return (
-    <CategoriesContainer>
-      <CategoriesOuterWrapper>
+    <CategoriesContainer blur={blur}>
+      <CategoriesOuterWrapper onScroll={handleScrool}>
         <CategoriesInnerWrapper>
           {categories &&
             categories.map((cat) => (
