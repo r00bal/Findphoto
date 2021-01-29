d04b48f94b77c35b62fd2ade442174efe7b94dca
r00bal <hal9000@MacBook-Pro-r00bal.local>
2021-01-29 17:54:57 +0100

    add trap focus

--- src/containers/ModalContainer.js
+++ src/containers/ModalContainer.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable camelcase */
 import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
@@ -14,12 +15,60 @@ export default function ModalContainer({ isOpen }) {
     `${BASE_API_URL_UNPLASH_PHOTO}/${id}?client_id=${process.env.REACT_APP_APIKEY}`
   );
 
-  console.log(data);
-
   const onModalClose = (e) => {
-    e.stopPropagation();
     history.goBack();
   };
+
+  const modalRef = useRef();
+  useEffect(() => {
+    function keyListener(e) {
+      const listener = keyListenersMap.get(e.keyCode);
+      return listener && listener(e);
+    }
+    document.addEventListener('keydown', keyListener);
+
+    return () => document.removeEventListener('keydown', keyListener);
+  });
+
+  const handleTabKey = (e) => {
+    const focusableModalElements = Array.from(
+      modalRef.current.querySelectorAll(
+        'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select, [tabindex]:not([tabindex="-1"])'
+      )
+    );
+    e.preventDefault();
+    const firstElement = focusableModalElements[0];
+    const lastElement = focusableModalElements[focusableModalElements.length - 1];
+    const active = document.activeElement;
+    const indexOfActiveElement = focusableModalElements.indexOf(active);
+
+    if (focusableModalElements.indexOf(active) === -1) {
+      firstElement.focus();
+    }
+    if (!e.shiftKey && indexOfActiveElement < focusableModalElements.length - 1) {
+      focusableModalElements[indexOfActiveElement + 1].focus();
+      return e.preventDefault();
+    }
+    if (!e.shiftKey && indexOfActiveElement === focusableModalElements.length - 1) {
+      firstElement.focus();
+      return e.preventDefault();
+    }
+
+    if (e.shiftKey && indexOfActiveElement === 0) {
+      lastElement.focus();
+      return e.preventDefault();
+    }
+    if (e.shiftKey && indexOfActiveElement > 0) {
+      focusableModalElements[indexOfActiveElement - 1].focus();
+      return e.preventDefault();
+    }
+  };
+
+  const keyListenersMap = new Map([
+    [27, onModalClose],
+    [9, handleTabKey],
+  ]);
+
   if (isLoading) {
     return <p>Loading...</p>;
   }
@@ -32,24 +81,26 @@ export default function ModalContainer({ isOpen }) {
     const { location, user, alt_description, urls } = data || {};
     return (
       <Modal isOpen={isOpen} role="dialog" aria-modal="true">
-        <Card>
-          <Card.Header>
-            <Card.Wrapper>
-              <Card.Title>{user && user.name}</Card.Title>
-              <Card.Text>@{user && user.instagram_username}</Card.Text>
-            </Card.Wrapper>
-            <Card.Button
-              autoFocus={isOpen}
-              onClick={(e) => {
-                onModalClose(e);
-              }}
-            >
-              CLOSE
-            </Card.Button>
-          </Card.Header>
-          <Card.Image src={urls.regular} alt={alt_description} />
-          <Card.Footer>{location.name}</Card.Footer>
-        </Card>
+        <div style={{ height: '100%', width: '100%' }} ref={modalRef}>
+          <Card>
+            <Card.Header>
+              <Card.Wrapper>
+                <Card.Title tabIndex="0">{user && user.name}</Card.Title>
+                <Card.Text>@{user && user.instagram_username}</Card.Text>
+              </Card.Wrapper>
+              <Card.Button
+                autoFocus={isOpen}
+                onClick={(e) => {
+                  onModalClose(e);
+                }}
+              >
+                CLOSE
+              </Card.Button>
+            </Card.Header>
+            <Card.Image src={urls.regular} alt={alt_description} tabIndex="0" />
+            <Card.Footer>{location.name}</Card.Footer>
+          </Card>
+        </div>
       </Modal>
     );
   }
