8d03e49ca227b76b1423f8298170d7429b3427c5
r00bal <hal9000@MacBook-Pro-r00bal.local>
2021-01-29 19:08:15 +0100

    centred Card

--- src/components/card/styles/Card.js
+++ src/components/card/styles/Card.js
@@ -10,6 +10,7 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+  margin: 0 auto;
 `;
 
 export const Button = styled.button`
