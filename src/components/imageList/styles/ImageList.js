a6c08bfdade9f080307aae9c31f5cfabd9b7def6
r00bal <hal9000@MacBook-Pro-r00bal.local>
2021-02-11 12:54:56 +0100

    add media

--- src/components/imageList/styles/ImageList.js
+++ src/components/imageList/styles/ImageList.js
@@ -2,8 +2,14 @@ import styled from 'styled-components/macro';
 
 export const ImageListWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(1, 1fr);
   grid-gap: 24px;
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
 `;
 
 export const ImageCardWrapper = styled.div`
